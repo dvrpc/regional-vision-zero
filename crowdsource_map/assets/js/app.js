46b717da10a3391bfedbcacfc37bcbd2905b35a8
Christopher Pollard <crvanpollard@gmail.com>
2025-02-16 14:12:14 -0500

    style: about and footer

--- crowdsource_map/assets/js/app.js
+++ crowdsource_map/assets/js/app.js
@@ -1,1 +1,13 @@
-$('#aboutModal').modal('show');
+// $('#aboutModal').modal('show');
+$(document).ready(function(){
+    $("#iframe_map").addClass("original-style"); // Initial style
+    $("#footer").addClass("footer-og"); // Initial style
+    $("#hide").addClass("fa fa-chevron-down fa-lg"); // Initial style
+
+    $("#hide").click(function(){
+      $(".footer_panel").toggle();
+      $("#iframe_map").toggleClass("original-style alternate-style");
+      $("#footer").toggleClass("footer-og footer-alt");
+      $("#hide").toggleClass("fa fa-chevron-down fa-lg fa fa-chevron-up fa-lg"); // Initial style
+    });
+  });
