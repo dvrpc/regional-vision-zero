e4a7e2ed5b6b33ba3ffb856a5af38cd8560e0244
Christopher Pollard <crvanpollard@gmail.com>
2025-02-21 09:15:33 -0500

    style: cleaning up

--- crowdsource_map/assets/js/app.js
+++ crowdsource_map/assets/js/app.js
@@ -1,13 +1,13 @@
 $('#aboutModal').modal('show');
 $(document).ready(function(){
-    $("#iframe_map").addClass("original-style"); // Initial style
-    $("#footer").addClass("footer-og"); // Initial style
-    $("#hide").addClass("fa fa-chevron-down fa-lg"); // Initial style
+    $("#iframe_map").addClass("alternate-style"); // Initial style
+    $("#footer").addClass("footer-alt"); // Initial style
+    $("#hide").addClass("fa fa-chevron-up fa-lg"); // Initial style
 
     $("#hide").click(function(){
-      $(".footer_panel").toggle();
-      $("#iframe_map").toggleClass("original-style alternate-style");
-      $("#footer").toggleClass("footer-og footer-alt");
-      $("#hide").toggleClass("fa fa-chevron-down fa-lg fa fa-chevron-up fa-lg"); // Initial style
+      // $(".footer_panel").toggle();
+      $("#iframe_map").toggleClass("alternate-style original-style");
+      $("#footer").toggleClass("footer-alt footer-og");
+      $("#hide").toggleClass("fa fa-chevron-up fa-lg fa fa-chevron-down fa-lg"); // Initial style
     });
   });
